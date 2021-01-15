a7cbcd62119ebd9ef6e332a0ef308a03a8f59d1a
chenm <chenm@youshu.cc>
2021-01-15 14:29:21 +0800

    修改udf的名称

--- src/main/java/udfhiveimpala/ArrayFeature.java
+++ src/main/java/udfhiveimpala/ArrayFeature.java
@@ -1,49 +1,49 @@
 package udfhiveimpala;
 
 import org.apache.hadoop.hive.ql.exec.UDF;
-
 import java.util.ArrayList;
 
-public class hiveArrayFeature extends UDF{
+public class ArrayFeature extends UDF{
 
-    // 将数组进行前后截取，提取有效行为时间
-    public static ArrayList<Integer> monthStatistics1(ArrayList<Integer> x){
+    // 1:将数组进行前后截取，提取有效行为时间
+    public static ArrayList<Integer> monthStatistics(ArrayList<Integer> x){
         int label = 0;
-        int start_index = 0;
-        int end_index = 0;
-        for (int i=0;i<x.size();i++){
+        int startIndex = 0;
+        int endIndex = 0;
+        for (int i = 0; i < x.size(); i++){
             int value = x.get(i);
             if(value !=0){
-                if(label==0){
-                    start_index = i;
-                    end_index = i;
+                if(label == 0){
+                    startIndex = i;
+                    endIndex = i;
                     label = 1;
                 }
                 else {
-                    end_index = i;
+                    endIndex = i;
                 }
             }
         }
         ArrayList<Integer> result = new ArrayList<Integer>();
-        for(int i=start_index;i<=end_index;i++){
+        for(int i = startIndex; i <= endIndex; i++){
             result.add(x.get(i));
         }
         return result;
     }
 
 
-    public static double sum_(ArrayList<Integer> x){
-        //2:求总数
+    //2:求总数
+    public static double sumFeature(ArrayList<Integer> x){
         int sum=0;
         int length = x.size();
-        for (int i=0;i<length;i++){
+        for (int i=0; i<length; i++){
             sum += x.get(i);
         }
         return sum;
     }
 
-    public static double mean_(ArrayList<Integer> x){
-        //3:求平均数
+
+    //3:求平均数
+    public static double meanFeature(ArrayList<Integer> x){
         int sum=0;
         int length = x.size();
         for (int i=0;i<length;i++){
@@ -52,20 +52,21 @@ public class hiveArrayFeature extends UDF{
         return (double)sum /length;
     }
 
+
+    //4:求波动性,偏度,峰度
     public static ArrayList bodongSkewKurt(ArrayList<Integer> x){
-        //4:求波动性,偏度,峰度
 
         //均值
-        double mean_value = 0.0;
-        for(int i=0;i<x.size();i++){
-            mean_value += x.get(i);
+        double meanValue = 0.0;
+        for(int i=0; i<x.size(); i++){
+            meanValue += x.get(i);
         }
-        mean_value = mean_value/x.size();
+        meanValue = meanValue/x.size();
 
         double sum = 0.0;
         double sum2 = 0.0;
         double sum3 = 0.0;
-        for(int i=0;i<x.size();i++){
+        for(int i = 0; i < x.size();i++){
             sum += x.get(i);
             sum2 += Math.pow(x.get(i),2);
             sum3 += Math.pow(x.get(i),3);
@@ -73,18 +74,23 @@ public class hiveArrayFeature extends UDF{
         sum = sum/x.size();  //均值
         sum2 = sum2/x.size(); //平方的均值
         sum3 = sum3/x.size(); //三次方的均值
-        double sigma = Math.sqrt(sum2 - sum*sum); //这是D(x)的开方，标准差
+
+        // 标准差：根号下（平方的均值-均值的平方）
+        double sigma = Math.sqrt(sum2 - sum * sum); //这是D(x)的开方，标准差
 
         //波动性 np.std(x)/(np.mean(x)*np.sqrt(len(x))
-        double budong = sigma/(mean_value*Math.sqrt(x.size()));
+        double budong = sigma / (meanValue * Math.sqrt(x.size()));
 
         double sum4 = 0.0;
-        for(int i=0;i<x.size();i++){
-            sum4 += Math.pow(x.get(i)-sum,4);
+        for(int i=0; i < x.size(); i++){
+            sum4 += Math.pow(x.get(i) - sum,4);
         }
         sum4 = sum4/x.size();
-        double skew = (sum3 - 3*sum*Math.pow(sigma,2) - Math.pow(sum,3))/Math.pow(sigma,3);
-        double kurt = sum4/Math.pow(sigma,2);
+        // 偏度
+        double skew = (sum3 - 3*sum * Math.pow(sigma,2) - Math.pow(sum,3)) / Math.pow(sigma,3);
+        // 峰度
+        double kurt = sum4 / Math.pow(sigma,2);
+
         ArrayList<Double> result = new ArrayList<Double>();
         result.add(budong);
         result.add(skew);
@@ -99,32 +105,34 @@ public class hiveArrayFeature extends UDF{
         String[] bb = y.split(",");
         ArrayList<Integer> result = new ArrayList<Integer>();
 
-        for (int i=0;i<12;i++){
+        for (int i=0; i<12; i++){
             result.add(0);
         }
-        for (int i=0;i<aa.length;i++){
+        for (int i=0; i<aa.length; i++){
             result.set(Integer.valueOf(aa[i].trim())-1,Integer.valueOf(bb[i].trim()));
         }
         return result;
     }
 
-    public static String main_evaluate(String x,String y){
+
+
+    public static String mainEvaluate(String x, String y){
         //开始整体计算特征值
         // 1:将输入的两个字符串转为原始12个月的数组
         ArrayList<Integer> xx = trans2Array(x,y);
         System.out.println(xx);
 
         // 2:将数组进行前后截取
-        ArrayList<Integer> yy = monthStatistics1(xx);
+        ArrayList<Integer> yy = monthStatistics(xx);
         System.out.println(yy);
 
         // 3：特征提取
         // 特征1：长度
         int len_ = yy.size();
         // 特征2：总量
-        double sum_s = sum_(yy);
+        double sum_s = sumFeature(yy);
         // 特征3：平均数
-        double mean_ = mean_(yy);
+        double mean_ = meanFeature(yy);
 
         // 特征4：波动性/偏度/峰度
         ArrayList<Double> skew_kurt_value = bodongSkewKurt(yy);
@@ -145,7 +153,7 @@ public class hiveArrayFeature extends UDF{
     public static void main(String[] args) {
         // x：有行为的月份
         // y: 对应月份的数值
-        main_evaluate("1,5","30,50");
+        mainEvaluate("1,5","30,50");
 
     }
 }
--- src/main/java/udfhiveimpala/TongjiFeatureMain.java
+++ src/main/java/udfhiveimpala/TongjiFeatureMain.java
@@ -4,25 +4,27 @@ import org.apache.hadoop.hive.ql.exec.UDF;
 
 import java.util.ArrayList;
 
-public class tongjiFeature extends UDF {
+public class TongjiFeatureMain extends UDF {
 
     public static String evaluate(String x,String y){
-        //开始整体计算特征值
+        //开始整体计算特征
         // 1:将输入的两个字符串转为原始12个月的数组
-        ArrayList<Integer> xx = hiveArrayFeature.trans2Array(x,y);
-        System.out.println(xx);
+        ArrayList<Integer> xx = ArrayFeature.trans2Array(x,y);
+        System.out.println("转化为12月："+xx);
+
         // 2:将数组进行前后截取
-        ArrayList<Integer> yy = hiveArrayFeature.monthStatistics1(xx);
-        System.out.println(yy);
+        ArrayList<Integer> yy = ArrayFeature.monthStatistics(xx);
+        System.out.println("截取有效月份："+yy);
+
         // 3：特征提取
         // 特征1：长度
         int len_ = yy.size();
         // 特征2：总量
-        double sum_s = hiveArrayFeature.sum_(yy);
+        double sum_s = ArrayFeature.sumFeature(yy);
         // 特征3：平均数
-        double mean_ = hiveArrayFeature.mean_(yy);
+        double mean_ = ArrayFeature.meanFeature(yy);
         // 特征4：波动性/偏度/峰度
-        ArrayList<Double> skew_kurt_value = hiveArrayFeature.bodongSkewKurt(yy);
+        ArrayList<Double> skew_kurt_value = ArrayFeature.bodongSkewKurt(yy);
         double budong = skew_kurt_value.get(0);
         double skew = skew_kurt_value.get(1);
         double kurt = skew_kurt_value.get(2);
@@ -33,11 +35,12 @@ public class tongjiFeature extends UDF {
                 String.format("%.2f",budong)+"|"+
                 String.format("%.2f",skew)+"|"+
                 String.format("%.2f",kurt);
-        System.out.println(result);
+        System.out.println("最终特征："+result);
         return result;
 
     }
     public static void main(String[] args){
+        // 4月份值为3 ；1月份值为5 (x为月份，y为对应的值)
         evaluate("4, 1, 3, 8, 5, 7, 6","3, 5, 9, 7, 1, 8, 1");
     }
 }
